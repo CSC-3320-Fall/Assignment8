a3c87454195ceb87f1d5a9f05997d5fc288dbbc9
Erik Orndahl <eorndahl@gmail.com>
2013-11-12 13:35:46 -0500

    enter input / output in cmd now

--- eorndahl1/CA_Distance.c
+++ eorndahl1/CA_Distance.c
@@ -8,10 +8,10 @@ int main(int argc, char** argv)
     char scan[1000], c0[100], c2[100], c3[100], c4[100];
     int count = 0, c1, c5, residue;
     float c6, c7, c8, x, y, z, distance;
-    
-    f = fopen("/Users/eorndahl/HW8/4HKD.pdb" , "r");
-    out = fopen("/Users/eorndahl/HW8/CSV.txt" , "w");
-    
+
+    f = fopen(argv[1] , "r");
+    out = fopen(argv[2] , "w");
+
     while ( fgets(scan, sizeof(scan), f) !=NULL) { //scan line by line
         fscanf(f, "%s %d %s %s %s %d %f %f %f", &c0, &c1, &c2, &c3, &c4, &c5, &c6, &c7, &c8);
         //scan each column in to a variable.
@@ -25,18 +25,18 @@ int main(int argc, char** argv)
                     residue = c5;
                     count++;
                 } else { //if second compute with first and current values.
-//                  printf("x1 = %f x0 = %f\n y1 = %f y0 = %f\n z1 = %f z0 = %f\n", c6, x, c7, y, c8, z); //if you care to check equation values
+//                  printf("x1 = %f x0 = %f\n y1 = %f y0 = %f\n z1 = %f z0 = %f\n", x, c6, y, c7, z, c8); //if you care to check equation values
                     distance = sqrt((pow((x - c6), 2) + pow((y - c7),2) + pow((z - c8), 2))); //cartesian x,y,z distance formula
                     fprintf(out, "%d, %d, %f\n", residue, c5, distance);
                     count++;
                     }
 
               }
-         }
+        }
     }
-    
+
     fclose(f);
     fclose(out);
-    
+
 return 0;
 }
