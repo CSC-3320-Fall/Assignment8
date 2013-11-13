ce7e80ca269e9a996f3f4371a9ebecb5e0f98d77
whorlwind <cvaughn5@student.gsu.edu>
2013-11-13 02:23:49 -0500

    extra credit completed

--- cvaughn5/ass8.c
+++ cvaughn5/ass8.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-void part2();
+int part2();
+void extracredit();
 int main(int argc, char *argv[]){
-        int filenumb,atoms=0,hetatms=0,resnum=0,oldresnum=0; //file count,atom/hetatm count, residue number current/previous
+        int filenumb,atoms=0,hetatms=0,resnum=0,oldresnum=0,cacount=0; //file count,atom/hetatm count, residue number current/previous
 		char line[500],header[6]; //current line being read, ATOM/HET
 		float loc[3]={0},oldloc[3]={0}; //arrays for current CA atoms location and previous CA atom
-		
+		float db1[3][50];
 		
 		for (filenumb=argc;filenumb >1;filenumb--){ //allows for multiple files to be read from arguments
 			
@@ -18,30 +19,25 @@ int main(int argc, char *argv[]){
 				
 				if (strcmp(header,"ATOM")==0) {
 					atoms++;
-					part2(outputcsv,line,&loc,&oldloc,&resnum,&oldresnum); //run part 2
-					oldloc[0]=loc[0]; //move new data to replace last entry
-					oldloc[1]=loc[1];
-					oldloc[2]=loc[2];
-					oldresnum=resnum;
+					cacount=part2(outputcsv,line,&loc,&oldloc,&resnum,&oldresnum,cacount,&db1); //run part 2
+					
 					
 				}
 				else if (strcmp(header,"HETATM")==0) {
 					hetatms++;
-					part2(outputcsv,line,&loc,&oldloc,&resnum,&oldresnum); //same as above
-					oldloc[0]=loc[0];
-					oldloc[1]=loc[1];
-					oldloc[2]=loc[2];
-					oldresnum=resnum;
+					cacount=part2(outputcsv,line,&loc,&oldloc,&resnum,&oldresnum,cacount,&db1); //same as above
+					
 				}
 			}
 			printf("File %s Atom count = %d\n",argv[filenumb-1],atoms);
 			printf("File %s HETAtom count = %d\n",argv[filenumb-1],hetatms);
 			atoms=0;
 			hetatms=0;
+			extracredit(&db1);
 		}
 }
 
-void part2(FILE *fp, char line[500],float (*location)[3],float (*oldlocation)[3],int (*resnumb),int *oldresnumb)
+int part2(FILE *fp, char line[500],float (*location)[3],float (*oldlocation)[3],int *resnumb,int *oldresnumb,int count,float (*db1)[3][50])
 {
 	float difference[3],distance; 
 	char ab[3]; //atom type to check for CA
@@ -56,8 +52,39 @@ void part2(FILE *fp, char line[500],float (*location)[3],float (*oldlocation)[3]
 		difference[2]=*location[2]-*oldlocation[2];
 		distance=sqrt(pow(difference[0],2)+pow(difference[1],2)+pow(difference[2],2)); //find distance
 		fprintf(fp,"%i,%i,%f\n",*oldresnumb,*resnumb,distance); //write to file
-		
+		*oldlocation[0]=*location[0]; //copy new CA values to old for comparison
+		*oldlocation[1]=*location[1];
+		*oldlocation[2]=*location[2];
+		(*db1)[0][count]=*location[0];
+		(*db1)[1][count]=*location[1];
+		(*db1)[2][count]=*location[2];
+		*oldresnumb=*resnumb;
+		return (count+1);
 	}
+	else return count;
 }
+ void extracredit(float (*db1)[3][50]){
+	int i=0,j=0,max=0,distance=0;
+	float difference[3]={0};
+	FILE *file1 = fopen("bonus.xpm", "w");
+	fprintf(file1,"static char * plaid[] = { \"50 50 8 1\",\n \"0 c #ffffff\",\"1 c #1b2246\", \"2 c #287A9C\"");
+	fprintf(file1,", \"3 c #289C81\", \"4 c #289C5F\", \"5 c #389C28\", \"6 c #B9CD4B\", \"7 c #C79D34\", \"8 c #C83810\",\n");
+	
+	for (i=0;i<50;i++){
+		fprintf(file1,"\"");
+		for (j=0;j<50;j++){
+		difference[0]=(*db1)[0][i]-(*db1)[0][j]; //find individual axis differences from previous CA atom
+		difference[1]=(*db1)[1][i]-(*db1)[1][j];
+		difference[2]=(*db1)[1][i]-(*db1)[1][j];
+		distance=(sqrt(pow(difference[0],2)+pow(difference[1],2)+pow(difference[2],2)))/10;
+		if (distance>max) max=distance;
+		fprintf(file1,"%i ",distance);
+		}
+		fprintf(file1,"\",\n");
+	}
+	fprintf(file1,"};");
+	
+}
+
 
 				
